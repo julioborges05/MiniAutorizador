0210e8211c814b5e10a2f3768f3f73ca8611db47
Júlio Borges <julioborges05@hotmail.com>
2026-01-12 02:20:37 -0300

    Adiciona validação na tabela de usuarios

--- src/main/java/com/julioborges/autorizador/config/SecurityConfiguration.java
+++ src/main/java/com/julioborges/autorizador/config/SecurityConfiguration.java
@@ -2,6 +2,8 @@ package com.julioborges.autorizador.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -37,6 +39,11 @@ public class SecurityConfiguration {
                         .accessDeniedHandler(customAccessDeniedHandler)
                 ).addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
 }
--- src/main/java/com/julioborges/autorizador/service/AuthService.java
+++ src/main/java/com/julioborges/autorizador/service/AuthService.java
@@ -1,12 +1,27 @@
 package com.julioborges.autorizador.service;
 
+import com.julioborges.autorizador.model.User;
+import com.julioborges.autorizador.repository.UserRepository;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
 
+    private final BCryptPasswordEncoder encoder;
+    private final UserRepository userRepository;
+
+    public AuthService(BCryptPasswordEncoder encoder, UserRepository userRepository) {
+        this.encoder = encoder;
+        this.userRepository = userRepository;
+    }
+
     public boolean validateLogin(String login, String password) {
-        return true; //TODO: Implementar metodo de login
+        User user = userRepository.findByLogin(login)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
+
+        return encoder.matches(password, user.getPassword());
     }
 
 }
